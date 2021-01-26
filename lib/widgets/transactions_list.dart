52aa6aadfbd478ddb6e2d155c64a55ac03cd450b
Casper Hauberg <casperhauberg@icloud.com>
2021-01-26 12:34:14 +0100

    95

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -34,10 +34,7 @@ class TransactionList extends StatelessWidget {
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                   Text(
                     transactions[index].title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
+                    style: Theme.of(context).textTheme.headline6,
                   ),
                   Text(
                     DateFormat('yyyy-MM-dd').format(transactions[index].date),
