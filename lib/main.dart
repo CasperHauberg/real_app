2460bc61785cae43807a236fd00ed87267475685
Casper Hauberg <casperhauberg@icloud.com>
2021-01-29 13:07:08 +0100

    121

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:real_app/widgets/chart.dart';
+// import 'package:flutter/services.dart';
 
+import 'package:real_app/widgets/chart.dart';
 import 'package:real_app/widgets/new_transaction.dart';
 import 'package:real_app/widgets/transactions_list.dart';
 import './models/transaction.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations(
+  //   [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  // );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
